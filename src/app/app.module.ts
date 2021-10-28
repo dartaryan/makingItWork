029e5a5aad8e433ce0ec4e199ad01cf76396fcde
dartaryan <76628877+dartaryan@users.noreply.github.com>
2021-10-28 22:18:38 +0300

    Understanding Angular Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RedComponent } from './red/red.component';
+import { BlueComponent } from './blue/blue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    RedComponent,
+    BlueComponent
   ],
   imports: [
     BrowserModule,
